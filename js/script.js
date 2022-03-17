e04fd9d12c6d8a4fca299ed351babdb6881af1a5
andreycr98 <santiagovr3111@gmail.com>
2022-03-17 01:09:44 -0600

    Commit 11: drag and drop functionality completed.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const fillings  = document.querySelectorAll('.filling ');
+const empties = document.querySelectorAll('.empty');
+let filling;
+
+
+for(const filling of fillings){
+    filling.addEventListener('dragstart', dragStart);
+    filling.addEventListener('dragend',dragEnd);
+}
+
+for(const empty of empties){
+    empty.addEventListener('dragover',dragOver);
+    empty.addEventListener('dragenter',dragEnter);
+    empty.addEventListener('dragleave',dragLeave);
+    empty.addEventListener('drop',dragDrop);
+
+
+}
+
+
+function dragStart(){
+    filling = this;
+    this.className += ' hold';
+    setTimeout(() => this.className = 'invisible',0);
+}
+
+function dragEnd(){
+    this.className = 'filling';
+}
+
+function dragOver(e){
+    console.log("over");
+    e.preventDefault();
+ 
+}
+
+function dragEnter(e){
+    console.log("enter");
+    this.className += ' hovered';
+}
+
+function dragLeave(){
+    console.log("leave");
+    this.className = 'empty';
+    
+}
+
+function dragDrop(){
+    this.className = 'empty';
+    if (!this.hasChildNodes()) {
+        this.append(filling);
+    }
+}
+
