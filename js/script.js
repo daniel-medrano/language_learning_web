1c7ac09febf0a26f0fa1745ecd3ce3d26baf9603
Genesis Delgado <gen.delvar@gmail.com>
2022-03-23 00:28:57 -0600

    Commit 16: fillings modification

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+//
 const fillings  = document.querySelectorAll('.filling ');
 const empties = document.querySelectorAll('.empty');
+const checkBttn = document.getElementById("check");
 let filling;
 
 
@@ -13,9 +15,30 @@ for(const empty of empties){
     empty.addEventListener('dragenter',dragEnter);
     empty.addEventListener('dragleave',dragLeave);
     empty.addEventListener('drop',dragDrop);
-
-
 }
+
+checkBttn.addEventListener("click", function() {
+    // For para iterar por sobre los primeros 3 empties.
+    for (let index = 0; index < 3; index++) {
+        // Se obtiene el empty de cada iteración
+        const empty = empties[index];
+        let isEmpty = empty.children == 0;
+        if (!isEmpty) {
+            const filling = empty.children[0];
+            let emptyId = empty.id.split("-")[0];
+            let fillingId = filling.id.split("-")[0];
+
+            let isMatch = emptyId == fillingId;
+
+            if (isMatch) {
+                filling.className += " correct";
+            } else {
+                filling.className += " incorrect";
+
+            }
+        }
+    }
+})
 
 
 function dragStart(){
@@ -44,7 +67,6 @@ function dragLeave(){
     this.className = 'empty';
     
 }
-
 
 function dragDrop(){
     this.className = 'empty';
